7d41a998ed272451c8637b3d1ccb7b06d5e7b953
Chris Brown <guardian.cat7585@farcemail.com>
2023-08-01 10:47:37 +1000

    update date time format

--- src/utils/formatter.ts
+++ src/utils/formatter.ts
@@ -1,6 +1,8 @@
-const dateFormatter = new Intl.DateTimeFormat('en-US', {
-  dateStyle: 'long',
-  timeStyle: 'short',
+const dateFormatter = new Intl.DateTimeFormat('en-AU', {
+  dateStyle: 'long', // 1 August 2023
+  timeStyle: 'long', // 10:15:14 am AEST
+  timeZone: 'Australia/Melbourne',
 })
+
 
 export const formatDate = (date: string) => dateFormatter.format(new Date(date))
