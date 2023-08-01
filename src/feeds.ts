7c9e1d8c4cf18cd1e4d643021595a4fdefe7a120
Chris Brown <guardian.cat7585@farcemail.com>
2023-08-01 14:07:52 +1000

    add feeds

--- src/feeds.ts
+++ src/feeds.ts
@@ -1,10 +1,21 @@
 export const feeds = [
-  // Daring Fireball
-  'https://daringfireball.net/feeds/main',
+  // *********************
+  // Technology
+  // *********************
 
-  // JakeArchibald.com
-  'https://jakearchibald.com/posts.rss',
+  'https://daringfireball.net/feeds/main', // Daring Fireball https://daringfireball.net/
 
-  // Appjeniksaan
-  'https://appjeniksaan.nl/feed.xml',
+  // *********************
+  // Space
+  // *********************
+
+  'https://blogs.nasa.gov/sunspot/feed/', // NASA Sun Spot https://blogs.nasa.gov/sunspot/
+  'https://blogs.nasa.gov/artemis/feed/', // NASA Artemis https://blogs.nasa.gov/artemis/
+  'https://blogs.nasa.gov/webb/feed/', // NASA JWST https://blogs.nasa.gov/webb/
+
+
+  // *********************
+  // Aviation 
+  // *********************
+  'https://www.avweb.com/feed/', // AvWeb https://www.avweb.com/
 ]
